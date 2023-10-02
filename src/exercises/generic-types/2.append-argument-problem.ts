4dcc4eccfd4bf57fe3d2640c04efd81b9e8c83a0
evondev <trananhtuan400@gmail.com>
2023-10-02 18:17:06 +0700

    update do exercise

--- src/exercises/generic-types/2.append-argument-problem.ts
+++
@@ -1,9 +0,0 @@
-import { Equal, Expect } from "@/type-utils";
-
-type SomeF = (a: number, b: string) => number;
-type AppendArgument<F, A> = unknown;
-
-type FinalF = AppendArgument<SomeF, boolean>;
-type tests = [
-  Expect<Equal<FinalF, (x: boolean, a: number, b: string) => number>>
-];
