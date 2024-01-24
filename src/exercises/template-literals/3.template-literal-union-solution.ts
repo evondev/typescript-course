8e957f006af2c9ec028a675ef959fba58b2f0242
evondev <trananhtuan400@gmail.com>
2024-01-24 22:53:25 +0700

    Add template literal exercises solutions

---
+++ src/exercises/template-literals/3.template-literal-union-solution.ts
@@ -0,0 +1,23 @@
+import { Equal, Expect } from "@/type-utils";
+
+type BreadType = "rye" | "brown" | "white";
+
+type Filling = "cheese" | "ham" | "salami";
+
+type Sandwich = `${BreadType} sandwich with ${Filling}`;
+type tests = [
+  Expect<
+    Equal<
+      Sandwich,
+      | "rye sandwich with cheese"
+      | "rye sandwich with ham"
+      | "rye sandwich with salami"
+      | "brown sandwich with cheese"
+      | "brown sandwich with ham"
+      | "brown sandwich with salami"
+      | "white sandwich with cheese"
+      | "white sandwich with ham"
+      | "white sandwich with salami"
+    >
+  >
+];
